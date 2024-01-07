05e180258867ab434a59c79b493057a4c41dbcf0
AnimaLibera <animalibera@mail.de>
2024-01-07 16:48:44 +0100

    Extract unique Countrys

--- Code/analyst.py
+++ Code/analyst.py
@@ -5,18 +5,29 @@ class Analyst:
     def __init__(self, start_date, end_date):
         self.raw_economic_calendar = pd.Provider().economic_calendar(start_date, end_date)
         self.raw_events = self.extract_events()
+        self.clean_high_impact_events = self.clean_events(self.extract_high_impact_events())
+        self.raw_countrys = self.extract_countrys()
+        self.unique_countrys = set(self.raw_countrys)
 
     def extract_events(self):
         """Extract Economic Calendar Events"""
         events = []
-        for event in self.raw_economic_calendar:
-            events.append(event["event"])
+        for element in self.raw_economic_calendar:
+            events.append(element["event"])
         return events
     
-    def clean_events(self):
+    def extract_high_impact_events(self):
+        """Extract High Impact Events"""
+        events = []
+        for element in self.raw_economic_calendar:
+            if element["impact"] == "High":
+                events.append(element["event"])
+        return events
+
+    def clean_events(self, raw_events):
         """Clean Economic Calendar Events"""
         events = []
-        for event in self.raw_events:
+        for event in raw_events:
             events.append(self.clean_event(event))
         return set(events)
 
@@ -34,3 +45,9 @@ class Analyst:
         else:
             return False
     
+    def extract_countrys(self):
+        """Extract Countries"""
+        countrys = []
+        for element in self.raw_economic_calendar:
+            countrys.append(element["country"])
+        return countrys 
--- Code/main.py
+++ Code/main.py
@@ -3,11 +3,13 @@ import analyst as al
 import json
 
 start_date = "2023-01-01"
-end_date = "2023-02-01"
+end_date = "2024-01-01"
 #provider = pd.Provider()
 #print(provider.fmp_token)
 #json_data = provider.economic_calendar(start_date, end_date)
 #print(json.dumps(json_data, indent=2))
 analyst = al.Analyst(start_date, end_date)
-for event in analyst.clean_events():
-    print(event)
+#for event in analyst.clean_high_impact_events:
+#    print(event)
+for country in analyst.unique_countrys:
+    print(country)
