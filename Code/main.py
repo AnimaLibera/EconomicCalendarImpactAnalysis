e62ba08856bdf409af5902690ab9ec8def60d050
AnimaLibera <animalibera@mail.de>
2024-01-07 18:05:30 +0100

    Get and parse Forex Rates

--- Code/main.py
+++ Code/main.py
@@ -4,12 +4,14 @@ import json
 
 start_date = "2023-01-01"
 end_date = "2024-01-01"
-#provider = pd.Provider()
+provider = pd.Provider()
+print(provider.foreign_exchange_rates(start_date, end_date))
+
 #print(provider.fmp_token)
 #json_data = provider.economic_calendar(start_date, end_date)
 #print(json.dumps(json_data, indent=2))
 analyst = al.Analyst(start_date, end_date)
 #for event in analyst.clean_high_impact_events:
 #    print(event)
-for country in analyst.unique_countrys:
-    print(country)
+#for country in analyst.unique_countrys:
+#    print(country)
--- Code/provider.py
+++ Code/provider.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import pandas as pd
 import requests
 import os
 
@@ -20,3 +21,23 @@ class Provider:
         json_data = response.json()
         
         return json_data
+
+    def foreign_exchange_rates(self, start_date, end_date, pair = "EURUSD"):
+        """Get Foreign Exchange Rates"""
+
+        endpoint = f"https://financialmodelingprep.com/api/v3/historical-chart/1min/{pair}?from={start_date}&to={end_date}&apikey={self.fmp_token}"
+        response = requests.get(endpoint)
+        json_data = response.json()
+
+        return self.parse_fx_rates(json_data)
+    
+    def parse_fx_rates(self, json_data):
+        """Parse Foreign Exchange Rates"""
+        rates = []
+        datetime = []
+
+        for element in json_data:
+            rates.append(element["close"])
+            datetime.append(element["date"])
+
+        return pd.Series(data=rates, index=datetime)
