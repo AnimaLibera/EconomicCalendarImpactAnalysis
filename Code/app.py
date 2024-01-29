ec84c30b2f01e024a68227d833a67a834cd18e2b
AnimaLibera <animalibera@mail.de>
2024-01-29 18:28:52 +0100

    Learn using Secrets in Streamlit

--- Code/app.py
+++ Code/app.py
@@ -1,3 +1,6 @@
 import streamlit as st
+import os
 
 st.write("Impact Analysis!")
+str.write(f"Secret Message: {os.environ['MESSAGE']} via OS")
+str.write(f"Secret Message: {st.secrets['MESSAGE']} via Streamlit")
