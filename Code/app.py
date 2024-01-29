4f6c7fcf865b95f92fd18bd40ae03c3265161eb3
AnimaLibera <animalibera@mail.de>
2024-01-29 18:37:06 +0100

    Use Secrets in f-String

--- Code/app.py
+++ Code/app.py
@@ -4,3 +4,5 @@ import os
 st.write("Impact Analysis!")
 st.write("Secret Message:", os.environ['MESSAGE'], "via OS")
 st.write("Secret Message:", st.secrets['MESSAGE'], "via Streamlit")
+st.write(f"Secret Message: {os.environ['MESSAGE']} via OS")
+st.write(f"Secret Message: {st.secrets['MESSAGE']} via Streamlit")
