4b1df75c990747a12c7188458ac9e2c02a82026f
AnimaLibera <animalibera@mail.de>
2024-02-01 20:36:54 +0100

    Test App-Connection to Database

--- Code/app.py
+++ Code/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import analyst as al
+import influx as db
 import pandas as pd
+
 
 title = "Impact Analysis"
 description = "This is an work in progress project to analyze the impact of indicators of the economic calendar on currency-pair prices."
@@ -17,7 +19,10 @@ def make_impact_analysis(_analyst):
     return analyst.new_impact_analysis(start, stop)
 
 data_load_state = st.text('Loading data...')
-impact_frame = make_impact_analysis(_analyst = analyst)
+#impact_frame = make_impact_analysis(_analyst = analyst)
 data_load_state.text('Loading data...done!')
-st.write(impact_frame)
+#st.write(impact_frame)
 st.write("Fooder")
+
+database = db.InfluxDatabase(deployment = "linode")
+st.write(database)
